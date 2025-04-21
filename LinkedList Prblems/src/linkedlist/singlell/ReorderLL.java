6c4f1977efd6be9e71f5e503c6139a3fd31273eb
Haripriya-511 <65435890+Haripriya-511@users.noreply.github.com>
2025-04-21 22:17:38 +0530

    Reorder LL

---
+++ LinkedList Prblems/src/linkedlist/singlell/ReorderLL.java
@@ -0,0 +1,58 @@
+package linkedlist.singlell;
+
+public class ReorderLL {
+
+    public static Node reverse(Node head){
+        if(head==null || head.next==null)
+            return head;
+        Node prev= null;
+        Node curr=head;
+        while(curr!=null){
+            Node temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    public static void reorderList(Node head) {
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        System.out.println(slow.data);
+        Node l1=slow.next;
+        slow.next=null;
+       l1=reverse(l1);
+        new SingleLinkedListImpl().printLinkedList(head);
+        new SingleLinkedListImpl().printLinkedList(l1);
+        Node temp=head;
+
+
+        while(l1!=null){
+            Node nextNode=temp.next;
+            Node nextNode2=l1.next;
+            temp.next=l1;
+            l1.next=nextNode;
+
+            temp=nextNode;
+            l1=nextNode2;
+        }
+        new SingleLinkedListImpl().printLinkedList(head);
+//        //return head;
+
+
+    }
+    public static void main(String[] args) {
+        SingleLinkedListImpl singleLinkedList=new SingleLinkedListImpl();
+        Node head1= singleLinkedList.arrayToLinkedList(new int[]{1,2});
+        Node head2= singleLinkedList.arrayToLinkedList(new int[]{1,2,3,4,5,6});
+        reorderList(head1);
+        reorderList(head2);
+
+
+
+    }
+}
